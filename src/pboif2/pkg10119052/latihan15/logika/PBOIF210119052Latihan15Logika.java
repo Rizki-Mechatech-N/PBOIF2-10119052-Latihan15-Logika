a3dd1d6cee25c6b33e39b80c0d694b616e47b46b
Kiki <Kiki@Kiki-PC>
2020-10-18 12:05:04 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan15/logika/PBOIF210119052Latihan15Logika.java
@@ -0,0 +1,23 @@
+package pboif2.pkg10119052.latihan15.logika;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * operasi logika
+ * 
+ */
+public class PBOIF210119052Latihan15Logika {
+
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
